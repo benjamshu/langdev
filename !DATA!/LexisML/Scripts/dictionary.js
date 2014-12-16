2788769bde983f9eca8bc0f2e33430519d486fb1
leaf-adventure <bjmns@me.com>
2014-12-16 01:15:20 -0800

    init setup

--- !DATA!/LexisML/Scripts/dictionary.js
+++ !DATA!/LexisML/Scripts/dictionary.js
@@ -1,17 +1,25 @@
 /* jslint asi:true, browser:true */
+/* global Lexis */
 
 var Dictionary = {
     Requester: new XMLHttpRequest(),
     init: function() {
-        Dictionary.lexis = Dictionary.Requester.responseXML;
+        var script = document.createElement("script");
+        script.src = "http://leaf.faint.xyz/!DATA!/master/!DATA!/LexisML/normalize.js";
+        document.head.appendChild(script);
+        document.addEventListener("DOMContentLoaded", function() {Dictionary.load(document.documentElement.dataset.src)}, false);
     },
     lexis: undefined,
     load: function(src) {
-        Dictionary.Requester.addEventListener("load", Dictionary.init, false);
+        Dictionary.Requester.addEventListener("load", Dictionary.setup, false);
         Dictionary.Requester.open("get", src, true);
         Dictionary.Requester.send();
     },
-    lemmas: {}
+    lemmas: {},
+    setup: function() {
+        Dictionary.lexis = Dictionary.Requester.responseXML;
+        Lexis.normalize(Dictionary.lexis);
+    }
 }
 
-
+Dictionary.init();
